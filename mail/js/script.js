e64e7b9170932cd7a257ec2b446f3cec74624e16
mariannacoppa <densimetra@gmail.com>
2024-06-28 16:52:51 +0200

    [UPD] insert flag and array cycle

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,5 +1,6 @@
 // definisco elenco di iscritti
 const iscritti = ["densimetra@gmail.com", "prova@gmail.com", "brugola@gmail.com", "mare@gmail.com"];
+console.log(iscritti);
 // recupero il pulsante dal dom
 const button = document.getElementById("submit");
 
@@ -7,5 +8,13 @@ button.addEventListener('click', function () {
     // recupero il valore inserito nel campo input
     let email = document.getElementById('email').value.toLowerCase();
     console.log(email);
-
+    // inserisco la variabile di controllo (flag)
+    let found_iscritti = false;
+    // ciclo l'array e confronto l'elemento da cercare con la mail inserita nel form
+    for (let i = 0; i < iscritti.length; i++) {
+        if(iscritti[i].toLowerCase() === email) {
+            // se la condizione è vera, imposto la variabile di controllo true
+            found_iscritti = true;
+        }
+    }
 });
