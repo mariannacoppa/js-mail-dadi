009ef8cefded9e25393ea0e19f6562b1ea63cf65
mariannacoppa <densimetra@gmail.com>
2024-06-28 17:50:28 +0200

    [UPD] add dice first conditional condition

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,6 +1,15 @@
-// creo un generatore di numeri random compresi tra 1 e 6 (escludendo i decimali) da attribuire al computer
-let pc_dice = Math.floor((Math.random() * 6 + 1));
-console.log(pc_dice);
-document.getElementById('pc_dice').innerText = pc_dice;
-// creo un generatore di numeri random compresi tra 1 e 6 (escludendo i decimali) da attribuire all'utente
-let human_dice = Math.floor((Math.random() * 6 + 1));
+// al click sul pulsante, vengono generati i due numeri random
+const button = document.getElementById('lancia');
+button.addEventListener('click', function () {
+    // creo un generatore di numeri random compresi tra 1 e 6 (escludendo i decimali) da attribuire al computer
+    let pc_dice = Math.floor((Math.random() * 6 + 1));
+    document.getElementById('pc_dice').innerText = pc_dice;
+    // creo un generatore di numeri random compresi tra 1 e 6 (escludendo i decimali) da attribuire all'utente
+    let human_dice = Math.floor((Math.random() * 6 + 1));
+    document.getElementById('human_dice').innerText = human_dice;
+    // assegno istruzioni condizionali
+    if (human_dice > pc_dice) {
+        // SE il numero utente è maggiore del numero computer, appare un messaggio contenente la scritta "hai vinto"
+        document.getElementById('result').innerText = "Hai vinto!";
+    }
+})
