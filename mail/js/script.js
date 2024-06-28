7746cad59413a13de20123cc32b31f7f067f6f2e
mariannacoppa <densimetra@gmail.com>
2024-06-28 16:34:51 +0200

    [UPD] defined array values

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,9 +1,11 @@
+// definisco elenco di iscritti
+const iscritti = ["densimetra@gmail.com", "prova@gmail.com", "brugola@gmail.com", "mare@gmail.com"];
 // recupero il pulsante dal dom
 const button = document.getElementById("submit");
 
 button.addEventListener('click', function () {
     // recupero il valore inserito nel campo input
     let email = document.getElementById('email').value.toLowerCase();
-    console.log(value);
-    
+    console.log(email);
+
 });
