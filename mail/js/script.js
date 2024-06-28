b28b8fb6456f804039c53e7df85f62928dec28ff
mariannacoppa <densimetra@gmail.com>
2024-06-28 15:57:02 +0200

    [UPD] create html form and save value from input field

---
+++ mail/js/script.js
@@ -0,0 +1,9 @@
+// recupero il pulsante dal dom
+const button = document.getElementById("submit");
+
+button.addEventListener('click', function () {
+    // recupero il valore inserito nel campo input
+    let email = document.getElementById('email').value.toLowerCase();
+    console.log(value);
+    
+});
